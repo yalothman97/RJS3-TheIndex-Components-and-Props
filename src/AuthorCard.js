d192c3d0c94693af001c6e7e526c002899954943
Octowl <alsaff1987@gmail.com>
2019-02-02 16:10:46 +0300

    Refactor authorName

--- src/AuthorCard.js
+++ src/AuthorCard.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class AuthorCard extends Component {
   render() {
     const author = this.props.author;
+    const authorName = `${author.first_name} ${author.last_name}`
     return (
       <div className="col-lg-4 col-md-6 col-12">
         <div className="card">
@@ -10,12 +11,12 @@ class AuthorCard extends Component {
             <img
               className="card-img-top img-fluid"
               src={author.imageUrl}
-              alt={author.first_name + " " + author.last_name}
+              alt={authorName}
             />
           </div>
           <div className="card-body">
             <h5 className="card-title">
-              <span>{author.first_name + " " + author.last_name}</span>
+              <span>{authorName}</span>
             </h5>
             <small className="card-text">{author.books.length} books</small>
           </div>
